90e4410717a2334875593f3d0c766a990874f004
Shruti <agarwal.shruti1302@gmail.com>
2020-05-02 16:53:25 +0530

    Initial commit

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+  const [isContracted, setContracted] = useState(true);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote(prevNote => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
+  function expand() {
+    setContracted(false);
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <form className="create-note">
+        {isContracted ? null : (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        )}
+        <textarea
+          name="content"
+          onChange={handleChange}
+          onClick={expand}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={isContracted ? "1" : "3"}
+        />
+        <Zoom in={isContracted ? null : "in"}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
